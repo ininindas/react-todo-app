4ab907ca0e4c9db31c4af29c0546eacc40db5acc
ininindas <ehdrh248@naver.com>
2024-06-12 11:43:58 +0900

    init

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,45 @@ import "./App.css";
 export default function App() {
   return (
     <>
-      <p>안녕</p>
+      <div className="header">
+        <h1>🎃Todo</h1>
+        <button>정보</button>
+      </div>
+
+      <form action="">
+        <input type="text" placeholder="write a todo" />
+      </form>
+
+      <div className="todos">
+        <div className="todo_item">
+          <input type="checkbox" name="" id="" />
+          안녕하세요
+          <div className="btns">
+            <button>수정</button>
+            <button>삭제</button>
+          </div>
+        </div>
+        <div className="todo_item">
+          <input type="checkbox" name="" id="" />
+          안녕하세요
+          <div className="btns">
+            <button>수정</button>
+            <button>삭제</button>
+          </div>
+        </div>
+        <div className="todo_item">
+          <input type="checkbox" name="" id="" />
+          안녕하세요
+          <div className="btns">
+            <button>수정</button>
+            <button>삭제</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="todo_botton">
+        <div></div>
+      </div>
     </>
   );
 }
